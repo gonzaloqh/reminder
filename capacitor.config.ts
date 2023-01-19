680cd136aa30380bc45cf18682e2b7a8cef649cb
gonzaloqh <gonzalo.qh.94@gmail.com>
2023-01-19 11:44:46 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Reminder',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;

9692cb04d42ff383c73706b5f1a72c72177a398e
gonzaloqh <gonzalo.qh.94@gmail.com>
2023-02-27 19:24:59 -0500

    final version

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -6,103 +6,36 @@ import { AlertController } from '@ionic/angular';
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
+
+
 export class Tab3Page {
 
-  data = {
-    miembros : [
-      {
-        nombre: "Gonzalo",
-        pendiente: 0,
-        gastos: [
-          {
-            nombre: "Comida1",
-            tipo: "P",
-            monto: 5
-          },
-          {
-            nombre: "Comida22",
-            tipo: "P",
-            monto: 10
-          },
-          {
-            nombre: "taxi",
-            tipo: "C",
-            monto: 3
-          },
-          {
-            nombre: "taxi",
-            tipo: "C",
-            monto: 2
-          },
-        ],
-        pagos: []
-      },
-      {
-        nombre: "Delia",
-        pendiente: 0,
-        gastos: [
-          {
-            nombre: "Comida1",
-            tipo: "P",
-            monto: 40
-          },
-          {
-            nombre: "Comida22",
-            tipo: "P",
-            monto: 30
-          },
-          {
-            nombre: "taxi",
-            tipo: "C",
-            monto: 3
-          },
-          {
-            nombre: "taxi",
-            tipo: "C",
-            monto: 2
-          },
-        ],
-        pagos: [
-          {
-            monto: 150
-          },
-        ]
-      },
-      {
-        nombre: "Eleana",
-        pendiente: 0,
-        gastos: [
-          {
-            nombre: "Comida1",
-            tipo: "P",
-            monto: 25
-          },
-          {
-            nombre: "Comida22",
-            tipo: "P",
-            monto: 45
-          },
-          {
-            nombre: "taxi",
-            tipo: "C",
-            monto: 3
-          },
-          {
-            nombre: "taxi",
-            tipo: "C",
-            monto: 2
-          },
-        ],
-        pagos: [
-          {
-            monto: 20
-          },
-        ]
-      }
-    ]
+  data : Data = {
+    miembros : []
   };
 
   constructor(private alertController: AlertController) {}
+
+  restaurarCalculo(){
+    this.data.miembros.forEach(miembro => {
+      miembro.pendiente = 0;
+    });
+  }
+
+  eliminar(miembro: string, nombre: string, tipo: string, monto : number){
+    for (var i = 0; i < this.data.miembros.length; i++) {      
+      if(miembro == this.data.miembros[i].nombre) {
+        //ELiminamos gasto
+        if (nombre.length > 0 || tipo.length > 0) {
+          this.data.miembros[i].gastos = this.data.miembros[i].gastos.filter(item => !(item.nombre == nombre && item.tipo == tipo && item.monto == monto));
+          return;
+        }        
+        //Eliminamos cobro
+        this.data.miembros[i].pagos = this.data.miembros[i].pagos.filter(item => item.monto !== monto);
+      }
+    }  
+    this.restaurarCalculo();  
+  }
 
   agregarMontoComun(nombrestr: string, monto: number){
     let n : number = this.data.miembros.length;
@@ -167,7 +100,8 @@ export class Tab3Page {
                   pagos: []
                 }
               );
-            }            
+            }
+            this.restaurarCalculo();
           },
         },
         {
@@ -181,7 +115,7 @@ export class Tab3Page {
         }
       ],
     });  
-    await alertInput.present();    
+    await alertInput.present();
   }
 
   agregarGastoPersonal(miembro:string, nombrestr: string, monto: number){
@@ -197,6 +131,21 @@ export class Tab3Page {
         );
       }
     }
+    this.restaurarCalculo();
+  }
+
+  agregarCobroPersonal(miembro:string, monto: number){
+    let n : number = this.data.miembros.length;
+    for (var i = 0; i < n; i++) {
+      if(this.data.miembros[i].nombre === miembro){
+        this.data.miembros[i].pagos.push(
+          {
+            monto: monto
+          }
+        );
+      }
+    }
+    this.restaurarCalculo();
   }
 
   async addGasto(miembro: string){
@@ -232,8 +181,38 @@ export class Tab3Page {
     await alertInput.present();  
   }
 
-  addCobro(miembro: string){
-    alert(miembro);
+  async addCobro(miembro: string){
+    const alertInput = await this.alertController.create({
+      header: 'Complete los datos',
+      buttons: [
+        {
+          text: 'Aceptar',
+          role: 'confirm',
+          handler: (dada) => {
+            if(Number(dada[0]) > 0){
+              this.agregarCobroPersonal(miembro, Number(dada[0]) - Number(dada[1]));
+            }            
+          },
+        },
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+        }
+      ],
+      inputs: [
+        {
+          type: 'number',
+          placeholder: 'Monto',
+          min: 1,          
+        },
+        {
+          type: 'number',
+          placeholder: 'Vuelto',
+          min: 0,          
+        },
+      ],
+    });  
+    await alertInput.present();  
   }
 
   calcular(){
@@ -275,3 +254,26 @@ export class Tab3Page {
     return pagos;
   }
 }
+
+
+
+export class Data  { 
+  public miembros: Miembro[] = [];
+}
+
+export class Miembro  { 
+  public nombre: string = "";
+  public pendiente: number = 0;
+  public gastos: Gasto[] = [];
+  public pagos: Pago[] = [];  
+} 
+
+export class Gasto  { 
+  public nombre: string = "";
+  public tipo: string = "";
+  public monto: number = 0;
+} 
+
+export class Pago  {
+  public monto: number = 0;
+} 
